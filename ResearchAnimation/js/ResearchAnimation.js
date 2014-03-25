ffb566fbf362e5aff6cfedd243cab0f4a0538f55
Francisco A. Granado Santos <francisco.granado1@upr.edu>
2014-03-24 21:27:33 -0400

    Add js and css floder

---
+++ ResearchAnimation/js/ResearchAnimation.js
@@ -0,0 +1,42 @@
+/**
+ * @author Joseph Ocasio and Francisco Granado
+ */
+var clickCounter=0;
+function initialize(){
+	
+	expand('medical_devices_block');
+}
+//Expands and contracts desired css
+function expand(div_block_id){
+	var id='#'+div_block_id;//ID received by function
+	//Original ID of style
+	var id_original_css=  {
+		backgroundColor: '#bca',
+	    width: '20px',
+	    border: '1px solid black',
+	    marginLeft:'0',
+	    fontSize:'1em',
+	    opacity:'100',
+	    borderWidth:'1px',
+	    borderHeight:'1px'
+	    
+	};
+	$(id).click(function(){
+		//If the click counter is even then animate back to original state
+		if(++clickCounter % 2 == 0){
+			$(this).animate(id_original_css,1500);
+		}else{
+			//Expand to desired css
+			$(this).animate({
+					width: "70%",
+			        opacity: 60,
+			        marginLeft: "0.6in",
+			        fontSize: "2em",
+			        borderWidth: "10px"},1500);
+		}
+	});
+	
+}
+$(document).ready(function(){
+	initialize();
+});
