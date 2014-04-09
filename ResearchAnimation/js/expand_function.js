adf3a9287993d5fd0c063a2630ca9f1d6a174bdc
Francisco A. Granado Santos <francisco.granado1@upr.edu>
2014-04-09 12:25:50 -0400

    Expand function fully functional and completed with debugging flags

--- ResearchAnimation/js/expand_function.js
+++ ResearchAnimation/js/expand_function.js
@@ -1,30 +1,66 @@
- 
-var counters = new Array();
-// Searches for every div that have an id attribute
-$('div').each(function(){
-			if(!this.id){}else{
-			var id = this.id +'_counter';
-				counters.push(id);//add id to counters array 
-				counters[id] = 0 ;//initialize as 0 the counter of corresponding id 
-			}
-			alert(counters);
-			alert(counters[0] +'='+counters[id]);
-		});
+function initialize() {
+    var id_original_css = {
+        backgroundColor: '#bca',
+        width: '30px',
+        border: '1px solid black',
+        marginLeft: '0',
+        fontSize: '1em',
+        opacity: '100',
+        borderWidth: '1px',
+        borderHeight: '1px'
+    };
+    var id_expanded_css = {
+        width: '100%',
+        opacity: 60,
+        marginLeft: '0.5in',
+        borderWidth: '5px'
+    };
 
-function expand(div_block_id,expansion_class,original_class,time,callback_function){
-	var id='#'+div_block_id;//ID received by function
-	counter = counters[div_block_id +'_counter']; //Associates the counter per div block respectively 
-	
-	//Original ID of style
-	$(id).click(function(){
-		//If the click counter is even then animate back to original state
-		//$( "#dialog" ).remove();
-		if(++counter % 2 == 0){
-			$(this).animate(original _class,time);
-		}else{
-			//Expand to desired css
-			$(this).animate(expansion_class,time);
-			//Reset counter on every uneven click so it wont get bigger
-			counters[div_block_id +'_counter'] = 0;
-		 }
+    var counters = new Array();
+    // Searches for every div that have an id attribute
+    $('div').each(function () {
+        if (!this.id) {} else {
+            var id = this.id + '_counter';
+            counters.push(id); //add id to counters array 
+            counters[id] = 0; //initialize as 0 the counter of corresponding id 
+            console.log(counters);//Logs the array
+        }
+    });
+	$(function() {
+	    	$( "#dialog" ).dialog();
+	    });
+    expand('medical_devices_block', id_expanded_css, id_original_css, 500);
+    expand('p_and_d_block', id_expanded_css, id_original_css, 500,function(){
+    	alert('ended');
+    });
+
+    function expand(div_block_id, expansion_class, original_class, time, callback_function) {
+        console.log("entered expand");
+        var id = '#' + div_block_id; //ID received by function
+        counter = counters[div_block_id + '_counter']; //Associates the counter per div block respectively 
+
+        //Original ID of style
+        $(id).click(function () {
+            console.log("entered expand on click");
+            //If the click counter is even then animate back to original state
+            $("#dialog").remove();
+            if (++counter % 2 == 0) {
+                console.log("entered expand if");
+                $(this).animate(original_class, time, callback_function);
+                console.log("animated original class");
+            } else {
+                console.log("entered expand else");
+                //Expand to desired css
+                $(this).animate(expansion_class, time, callback_function);
+                console.log("animated expand class");
+                //Reset counter on every uneven click so it wont get bigger
+                counters[div_block_id + '_counter'] = 0;
+            }
+            console.log("exited expand");
+        });
+    };
+}
+
+$(document).ready(function () {
+    initialize();
 });
